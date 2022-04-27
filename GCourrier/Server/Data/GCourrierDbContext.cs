eb188608e423cd2a92aa281b6ddff270a6aa6719
Driss <idrissisamm@gmail.com>
2022-04-27 12:57:14 +0100

    Migrate Database and Seeding Data

--- GCourrier/Server/Data/GCourrierDbContext.cs
+++ GCourrier/Server/Data/GCourrierDbContext.cs
@@ -14,6 +14,84 @@ namespace GCourrier.Server.Data
             : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<GCourrier.Shared.Department>()
+              .Property(Department => Department.Id).ValueGeneratedOnAdd();
+            //Seed Departments Table
+            modelBuilder.Entity<Department>().HasData(
+                new Department { Id = 1, Name = "IT" });
+            modelBuilder.Entity<Department>().HasData(
+                new Department { Id = 2, Name = "HR" });
+            modelBuilder.Entity<Department>().HasData(
+                new Department { Id = 3, Name = "Marketing" });
+            modelBuilder.Entity<Department>().HasData(
+                new Department { Id = 4, Name = "Admin" });
+
+
+
+
+
+            modelBuilder.Entity<GCourrier.Shared.Agent>()
+               .Property(Agent => Agent.Id).ValueGeneratedOnAdd();
+            // Seed Agent Table
+            modelBuilder.Entity<Agent>().HasData(new Agent
+            {
+
+                Id=1,
+                FirstName = "Abd",
+                LastName = "ElMonem",
+                Email = "abd@samobay.com",
+                
+                Power = Power.SuperUser,
+                DepartmentId = 1,
+                PhotoPath = "images/abd.png"
+            });
+
+            modelBuilder.Entity<Agent>().HasData(new Agent
+            {
+                Id = 2,
+                FirstName = "Said",
+                LastName = "Sassi",
+                Email = "said@amobay.com",
+                
+                Power = Power.SuperUser,
+                DepartmentId = 2,
+                PhotoPath = "images/said.jpg"
+            });
+
+            modelBuilder.Entity<Agent>().HasData(new Agent
+            {
+                Id = 3,
+                FirstName = "Aymen",
+                LastName = "Aymen",
+                Email = "aymen@samobay.com",
+                
+                Power = Power.SuperUser,
+                DepartmentId = 3,
+                PhotoPath = "images/aymen.png"
+            });
+
+            modelBuilder.Entity<Agent>().HasData(new Agent
+            {
+                Id = 4,
+                FirstName = "Idriss",
+                LastName = "Laabidi",
+                Email = "idriss@samobay.com",
+                
+                Power = Power.Admin,
+                DepartmentId = 4,
+                PhotoPath = "images/idriss.png"
+            });
+
+
+
+
+
+
+
+        }
 
         public DbSet<GCourrier.Shared.Agent> Agent { get; set; }
 
---
+++ GCourrier/Server/Migrations/20220427115526_InitialMigration.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GCourrier.Server.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Agent",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Power = table.Column<int>(type: "int", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false),
+                    PhotoPath = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agent", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Agent_Department_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Department",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Department",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "IT" },
+                    { 2, "HR" },
+                    { 3, "Marketing" },
+                    { 4, "Admin" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Agent",
+                columns: new[] { "Id", "DepartmentId", "Email", "FirstName", "LastName", "PhotoPath", "Power" },
+                values: new object[,]
+                {
+                    { 1, 1, "abd@samobay.com", "Abd", "ElMonem", "images/abd.png", 1 },
+                    { 2, 2, "said@amobay.com", "Said", "Sassi", "images/said.jpg", 1 },
+                    { 3, 3, "aymen@samobay.com", "Aymen", "Aymen", "images/aymen.png", 1 },
+                    { 4, 4, "idriss@samobay.com", "Idriss", "Laabidi", "images/idriss.png", 2 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agent_DepartmentId",
+                table: "Agent",
+                column: "DepartmentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Agent");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+        }
+    }
+}
