3cfdb9b0b6efec150e8eca5af688b2e92b3fc7ed
Driss <idrissisamm@gmail.com>
2022-04-27 21:36:44 +0100

    Agent Client Side Service

---
+++ GCourrier/Client/Services/AgentService.cs
@@ -0,0 +1,51 @@
+﻿using GCourrier.Shared;
+using System.Net.Http.Json;
+
+namespace GCourrier.Client.Services
+{
+    public class AgentService : IAgentService
+    {
+        private readonly HttpClient httpClient;
+
+        public AgentService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<Agent>> GetAgents()
+        {
+            return await httpClient.GetFromJsonAsync<IEnumerable<Agent>>("api/Agents");
+        }
+
+        public Task<Agent> AddAgent(Agent agent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAgent(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Agent> GetAgent(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Agent> GetAgentByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Agent>> Search(string name, Power? power)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Agent> UpdateAgent(Agent agent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
--- GCourrier/Client/Services/IAgentService.cs
+++ GCourrier/Client/Services/IAgentService.cs
@@ -1,12 +1,12 @@
 ﻿using GCourrier.Shared;
 
-namespace GCourrier.Server.Models
+namespace GCourrier.Client.Services
 {
-    public interface IAgentRepository
+    public interface IAgentService
     {
         Task<IEnumerable<Agent>> Search(string name, Power? power);
         Task<IEnumerable<Agent>> GetAgents();
-        Task<Agent> GetAgent(int Id);
+        Task<Agent> GetAgent(int AgentId);
         Task<Agent> GetAgentByEmail(string email);
         Task<Agent> AddAgent(Agent agent);
         Task<Agent> UpdateAgent(Agent agent);
