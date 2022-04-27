a79f1d6051bae2f6adb8579811d918bec1bdb71b
Driss <idrissisamm@gmail.com>
2022-04-27 10:36:42 +0100

    Add project files.

---
+++ GCourrier/Shared/Power.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCourrier.Shared
+{
+    public enum Power
+    {
+        User, SuperUser, Admin
+    }
+}
